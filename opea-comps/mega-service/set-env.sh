e0c6adc71ac58f863d51337d5c68239dd20e03f2
David Nachman <david@nachman.org>
2025-02-16 12:53:14 -0500

    checkpoint

---
+++ opea-comps/mega-service/set-env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export host_ip=127.0.0.1
+export OPEA_COMPS=~/work/GenAIComps
+
+
+export OLLAMA_HOST=http://${host_ip}:11434
---
+++ opea-comps/mega-service/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# set environment variables
+source ./set-env.sh
+
+# set the current working directory
+cwd=$(pwd)
+
+# pull the model to ollama
+# ollama pull llama3.2
+
+# validate the model
+# curl http://${host_ip}:11434/api/generate -d '{"model": "llama3.2", "prompt":"What is Deep Learning?"}'
+
+cd $OPEA_COMPS
+docker build --no-cache -t opea/dataprep-redis:latest --build-arg https_proxy=$https_proxy \
+  --build-arg http_proxy=$http_proxy -f comps/dataprep/redis/langchain/Dockerfile .
